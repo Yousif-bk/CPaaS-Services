e5fcca5685601bbe688daef6553b4160d426cde7
Yousif <yusuf.babiker0@gmail.com>
2025-05-02 10:30:30 +0400

    Added Whatsapp API

--- Object Repository/Nexus Interfaces OR/Nexus Whatsapp Send QR.rs
+++ Object Repository/Nexus Interfaces OR/Nexus Whatsapp Send QR.rs
@@ -1,18 +1,18 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <WebServiceRequestEntity>
    <description></description>
-   <name>Nexus Auth API OR</name>
+   <name>Nexus Whatsapp Send QR</name>
    <tag></tag>
-   <elementGuidId>ee72a451-8eb7-4af4-93cb-b92c135d4e99</elementGuidId>
+   <elementGuidId>0489162e-0ec9-4b73-a60b-d5fd12196066</elementGuidId>
    <selectorMethod>BASIC</selectorMethod>
    <smartLocatorEnabled>false</smartLocatorEnabled>
    <useRalativeImagePath>false</useRalativeImagePath>
    <autoUpdateContent>false</autoUpdateContent>
-   <connectionTimeout>0</connectionTimeout>
+   <connectionTimeout>-1</connectionTimeout>
    <followRedirects>true</followRedirects>
    <httpBody></httpBody>
    <httpBodyContent>{
-  &quot;text&quot;: &quot;{\n    \&quot;email\&quot; : \&quot;Beerappa.n+4@comviva.com\&quot;,\n    \&quot;password\&quot; :\&quot;1234@That\&quot;\n}&quot;,
+  &quot;text&quot;: &quot;{\n    \&quot;from\&quot;: \&quot;447520649997\&quot;,\n    \&quot;to\&quot;: \&quot;971554573936\&quot;,\n    \&quot;category\&quot;: \&quot;Marketing\&quot;,\n    \&quot;template\&quot;: \&quot;emailtest\&quot;,\n    \&quot;country\&quot;: \&quot;AE\&quot;,\n    \&quot;language\&quot;: \&quot;en\&quot;\n}&quot;,
   &quot;contentType&quot;: &quot;application/json&quot;,
   &quot;charset&quot;: &quot;UTF-8&quot;
 }</httpBodyContent>
@@ -23,22 +23,37 @@
       <name>Content-Type</name>
       <type>Main</type>
       <value>application/json</value>
-      <webElementGuid>7fcfd0e7-e0f4-4263-aec7-6c6492fd281d</webElementGuid>
+      <webElementGuid>20c2a73b-da59-4897-b6ba-9e56b3c04498</webElementGuid>
+   </httpHeaderProperties>
+   <httpHeaderProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>Authorization</name>
+      <type>Main</type>
+      <value>Bearer ${access_token}</value>
+      <webElementGuid>bd65c484-a9b3-475d-a766-925582b6939e</webElementGuid>
    </httpHeaderProperties>
    <katalonVersion>10.1.0</katalonVersion>
-   <maxResponseSize>0</maxResponseSize>
+   <maxResponseSize>-1</maxResponseSize>
    <migratedVersion>5.4.1</migratedVersion>
    <path></path>
    <restRequestMethod>POST</restRequestMethod>
-   <restUrl>https://nexus.eandenterprise.com/api/v1/accounts/users/login</restUrl>
+   <restUrl>https://nexus.eandenterprise.com/api/v1/whatsapp/send</restUrl>
    <serviceType>RESTful</serviceType>
    <soapBody></soapBody>
    <soapHeader></soapHeader>
    <soapRequestMethod></soapRequestMethod>
    <soapServiceEndpoint></soapServiceEndpoint>
    <soapServiceFunction></soapServiceFunction>
-   <socketTimeout>0</socketTimeout>
+   <socketTimeout>-1</socketTimeout>
    <useServiceInfoFromWsdl>true</useServiceInfoFromWsdl>
+   <variables>
+      <defaultValue>''</defaultValue>
+      <description></description>
+      <id>0b9d1483-fd3d-4f37-8270-adf85db304c4</id>
+      <masked>false</masked>
+      <name>access_token</name>
+   </variables>
    <verificationScript>import static org.assertj.core.api.Assertions.*
 
 import com.kms.katalon.core.testobject.RequestObject
